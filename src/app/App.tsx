84d9b08f8ed8dfe070e7e7c558db6de1f16a16df
woohm402 <woohm404@gmail.com>
2023-09-20 08:18:08 +0900

    추가/수정/삭제 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,6 +12,7 @@ import { ReviewItem } from "./components/ReviewItem";
 import "./reset.css";
 import "./App.css";
 import { AddReviewModal } from "./components/AddReviewModal";
+import { Modal } from "./components/Modal";
 
 const initialData = data.map(
   ({ content, id, image, rating, snack_name }): Review => {
@@ -28,6 +29,11 @@ const initialData = data.map(
 export const App = () => {
   const [reviews, setReviews] = useState<Review[]>(initialData);
   const [isAddModalOpen, setAddModalOpen] = useState(false);
+  const [reviewState, setReviewState] = useState<
+    | { state: "idle" }
+    | { state: "edit"; id: number; draft: string }
+    | { state: "delete"; id: number }
+  >({ state: "idle" });
 
   return (
     <div className="wrapper">
@@ -49,14 +55,46 @@ export const App = () => {
           <li key={review.id}>
             <ReviewItem
               review={review}
-              onChangeReview={(newReview) =>
-                setReviews(
-                  reviews.map((r) => (r.id === newReview.id ? newReview : r))
-                )
+              onEditReview={() =>
+                setReviewState({
+                  state: "edit",
+                  id: review.id,
+                  draft: review.content,
+                })
               }
               onDeleteReview={() =>
-                setReviews(reviews.filter((r) => r.id !== review.id))
+                setReviewState({ state: "delete", id: review.id })
               }
+              state={(() => {
+                if (reviewState.state === "idle") return { state: "idle" };
+                if (reviewState.state === "delete") return { state: "blocked" };
+                if (
+                  reviewState.state === "edit" &&
+                  reviewState.id !== review.id
+                )
+                  return { state: "blocked" };
+                return {
+                  state: "editing",
+                  draft: reviewState.draft,
+                  onChange: (e: string) =>
+                    setReviewState({ state: "edit", id: review.id, draft: e }),
+                  onChangeSubmit: () => {
+                    try {
+                      const newReview = {
+                        ...review,
+                        content: createReviewContent(reviewState.draft),
+                      };
+                      setReviews(
+                        reviews.map((r) => (r.id === review.id ? newReview : r))
+                      );
+                      setReviewState({ state: "idle" });
+                    } catch (err) {
+                      //
+                    }
+                  },
+                  onChangeCancel: () => setReviewState({ state: "idle" }),
+                };
+              })()}
             />
           </li>
         ))}
@@ -73,6 +111,41 @@ export const App = () => {
         onClose={() => setAddModalOpen(false)}
         onAddReview={(newReview) => setReviews([newReview, ...reviews])}
       />
+      <Modal
+        isOpen={reviewState.state === "delete"}
+        onClose={() => setReviewState({ state: "idle" })}
+      >
+        <h2>리뷰 삭제</h2>
+        {reviewState.state === "delete" &&
+          (() => {
+            const deleting = reviews.find((r) => r.id === reviewState.id);
+
+            if (!deleting) return; // cannot reach here
+
+            return (
+              <>
+                <p>"{deleting.snackName}"에 대한 리뷰를 삭제하시겠습니까?</p>
+                <div>
+                  <button
+                    data-testid="delete-review-delete"
+                    onClick={() => {
+                      setReviewState({ state: "idle" });
+                      setReviews(reviews.filter((r) => r.id !== deleting.id));
+                    }}
+                  >
+                    삭제
+                  </button>
+                  <button
+                    data-testid="delete-review-cancel"
+                    onClick={() => setReviewState({ state: "idle" })}
+                  >
+                    취소
+                  </button>
+                </div>
+              </>
+            );
+          })()}
+      </Modal>
     </div>
   );
 };
--- src/app/components/ReviewItem/index.tsx
+++ src/app/components/ReviewItem/index.tsx
@@ -3,9 +3,22 @@ import "./index.css";
 
 export const ReviewItem = ({
   review,
+  onDeleteReview,
+  state,
+  onEditReview,
 }: {
   review: Review;
-  onChangeReview: (newReview: Review) => void;
+  state:
+    | { state: "idle" }
+    | {
+        state: "editing";
+        draft: string;
+        onChange: (e: string) => void;
+        onChangeSubmit: () => void;
+        onChangeCancel: () => void;
+      }
+    | { state: "blocked" };
+  onEditReview: () => void;
   onDeleteReview: () => void;
 }) => {
   return (
@@ -16,8 +29,42 @@ export const ReviewItem = ({
           <strong>{review.snackName}</strong>
           <span>/</span>
           <b>*{review.rating.toFixed(1)}</b>
+          {state.state === "idle" ? (
+            <div className="reviewActions">
+              <button data-testid="edit-review" onClick={onEditReview}>
+                수정
+              </button>
+              <button data-testid="delete-review" onClick={onDeleteReview}>
+                삭제
+              </button>
+            </div>
+          ) : state.state === "editing" ? (
+            <div>
+              <button
+                data-testid="edit-review-save"
+                onClick={state.onChangeSubmit}
+              >
+                저장
+              </button>
+              <button
+                data-testid="edit-review-cancel"
+                onClick={state.onChangeCancel}
+              >
+                취소
+              </button>
+            </div>
+          ) : null}
         </div>
-        <p className="reviewContent">{review.content}</p>
+        {state.state === "editing" ? (
+          <textarea
+            data-testid="edit-review-content-input"
+            className="reviewContent"
+            value={state.draft}
+            onChange={(e) => state.onChange(e.target.value)}
+          />
+        ) : (
+          <p className="reviewContent">{review.content}</p>
+        )}
       </div>
     </div>
   );
