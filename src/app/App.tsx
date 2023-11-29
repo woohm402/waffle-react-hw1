e868558d738ba33a75a38e297796d09551cc7233
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-11-29 20:59:21 +0900

    ✨ react-router-dom 셋업, 페이지 기능 추가, 404 추가 (#6)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,98 +1,46 @@
 import './reset.css';
-import './App.css';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { createReviewContent, type Review } from '../entities/review';
+import { createReviewId, type Review } from '../entities/review';
 import { type Snack } from '../entities/snack';
-import { AddReviewModal } from './components/AddReviewModal';
-import { DeleteReviewModal } from './components/DeleteReviewModal';
-import { ReviewItem } from './components/ReviewItem';
+import { Layout } from './components/Layout';
 import { storeContext } from './contexts/storeContext';
+import { ReviewPage } from './pages/ReviewPage';
 
 export const App = () => {
   const [reviews, setReviews] = useState<Review[]>([]);
   const [snacks] = useState<Snack[]>([]);
-  const [isAddModalOpen, setAddModalOpen] = useState(false);
-  const [reviewState, setReviewState] = useState<
-    { state: 'idle' } | { state: 'edit'; id: number; draft: string } | { state: 'delete'; id: number }
-  >({ state: 'idle' });
+
+  const router = createBrowserRouter([
+    { path: '/', element: <ReviewPage /> },
+    { path: '*', element: <div>404</div> },
+  ]);
 
   return (
-    <storeContext.Provider value={{ reviews, snacks }}>
-      <div className="wrapper">
-        <header data-testid="header" className="header">
-          <a href="https://wafflestudio.com">
-            <img
-              className="logo"
-              src="https://www.wafflestudio.com/static/images/logo/waffle_logo_title.png"
-              data-testid="waffle-logo"
-              alt="와플스튜디오"
-            />
-            <h1 className="title" data-testid="header-title">
-              과자 리뷰
-            </h1>
-          </a>
-        </header>
-        <ul className="reviewList" data-testid="review-list">
-          {reviews.map((review) => (
-            <li key={review.id}>
-              <ReviewItem
-                review={review}
-                onEditReview={() =>
-                  setReviewState({
-                    state: 'edit',
-                    id: review.id,
-                    draft: review.content,
-                  })
-                }
-                onDeleteReview={() => setReviewState({ state: 'delete', id: review.id })}
-                state={(() => {
-                  if (reviewState.state === 'idle') return { state: 'idle' };
-                  if (reviewState.state === 'delete') return { state: 'blocked' };
-                  if (reviewState.state === 'edit' && reviewState.id !== review.id) return { state: 'blocked' };
-                  return {
-                    state: 'editing',
-                    draft: reviewState.draft,
-                    onChange: (e: string) => setReviewState({ state: 'edit', id: review.id, draft: e }),
-                    onChangeSubmit: () => {
-                      try {
-                        const newReview = {
-                          ...review,
-                          content: createReviewContent(reviewState.draft),
-                        };
-                        setReviews(reviews.map((r) => (r.id === review.id ? newReview : r)));
-                        setReviewState({ state: 'idle' });
-                      } catch (err) {
-                        //
-                      }
-                    },
-                    onChangeCancel: () => setReviewState({ state: 'idle' }),
-                  };
-                })()}
-              />
-            </li>
-          ))}
-        </ul>
-        <button data-testid="write-review" className="addModal" onClick={() => setAddModalOpen(true)}>
-          +
-        </button>
-        <AddReviewModal
-          isOpen={isAddModalOpen}
-          onClose={() => setAddModalOpen(false)}
-          onAddReview={(newReview) => setReviews([newReview, ...reviews])}
-        />
-        <DeleteReviewModal
-          reviewItem={reviewState.state === 'delete' ? reviews.find((r) => r.id === reviewState.id) ?? null : null}
-          onClose={() => setReviewState({ state: 'idle' })}
-          onDelete={() => {
-            if (reviewState.state !== 'delete') return;
-
-            setReviews(reviews.filter((r) => r.id !== reviewState.id));
-            setReviewState({ state: 'idle' });
-          }}
-        />
-      </div>
+    <storeContext.Provider
+      value={{
+        reviews,
+        snacks,
+        updateReview: useCallback(
+          (id: Review['id'], data: Partial<Omit<Review, 'id'>>) =>
+            setReviews((reviews) => reviews.map((r) => (r.id === id ? { ...r, ...data } : r))),
+          [],
+        ),
+        createReview: useCallback(
+          (data: Omit<Review, 'id'>) => setReviews((reviews) => [{ ...data, id: createReviewId() }, ...reviews]),
+          [],
+        ),
+        deleteReview: useCallback(
+          (id: Review['id']) => setReviews((reviews) => reviews.filter((r) => r.id !== id)),
+          [],
+        ),
+      }}
+    >
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
     </storeContext.Provider>
   );
 };
