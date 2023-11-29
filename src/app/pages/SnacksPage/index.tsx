53d79166431e4d216b8f89570d001d0a0cc6d1c8
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-11-29 22:25:47 +0900

    ✨ 과자 목록 페이지 구현 (#11)

--- src/app/pages/SnacksPage/index.tsx
+++ src/app/pages/SnacksPage/index.tsx
@@ -1,3 +1,22 @@
+import { Link } from 'react-router-dom';
+
+import { SnackCard } from '../../components/SnackCard';
+import { storeContext } from '../../contexts/storeContext';
+import { useTypedContext } from '../../hooks/useTypedContext';
+import styles from './index.module.css';
+
 export const SnacksPage = () => {
-  return <>과자</>;
+  const { snacks } = useTypedContext(storeContext);
+
+  return (
+    <ul className={styles.list}>
+      {snacks.map((s) => (
+        <li key={s.id}>
+          <Link to={`/snacks/${s.id}`}>
+            <SnackCard snack={s} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
