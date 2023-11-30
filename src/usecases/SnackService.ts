a2af0f08e5dbb54c0ff9283775f19158c7e1a26d
woohm402 <woohm404@gmail.com>
2023-12-01 00:53:41 +0900

    과자 생성 시 validation

--- src/usecases/SnackService.ts
+++ src/usecases/SnackService.ts
@@ -4,4 +4,9 @@ export type SnackService = {
   listSnacks: () => Promise<Snack[]>;
   getSnack: (snackId: Snack['id']) => Promise<Snack>;
   createSnack: (snack: Pick<Snack, 'src' | 'title'>) => Promise<Snack>;
+  validateSnack: (
+    snackForm: Partial<{ src: string; title: string }>,
+  ) =>
+    | { valid: false; errors: Partial<{ src: string; title: string }> }
+    | { valid: true; snack: Pick<Snack, 'src' | 'title'> };
 };
